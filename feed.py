8767d1cf3668afdff51f762fbf9edb90cca98525
Rohit Bamble <92783307+dweebxoxo@users.noreply.github.com>
2023-08-09 06:15:02 +0000

    breakpoint

--- feed.py
+++ feed.py
@@ -1,7 +1,8 @@
-import yaml
 import xml.etree.ElementTree as xml_tree
+import yaml
 
 with open('feed.yaml', 'r') as file:
+
     yaml_data = yaml.safe_load(file)
 
     rss_element = xml_tree.Element('rss', {'version':'2.0',
